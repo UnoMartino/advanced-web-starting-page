93f7f4462e5e7ad6cccfd1b9287ffcb5a21837b2
UnoMartino <69631058+UnoMartino@users.noreply.github.com>
2020-08-13 16:50:05 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -15,22 +15,25 @@
         <meta charset="utf-8"/>
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
         <link rel="stylesheet" href="index.css" type="text/css" />
+        <title>Start</title>
 
     </head>
     <body>
-        <div id="loginform">
-            <form action="zaloguj.php" method="post">
-                Login lub email: <br/> <input type="text" name="login" /> <br/>
-                Hasło: <br/> <input type="password" name="password"/> <br/> <br/>
-                <div id="button" ><input type="submit" value="Zaloguj się"/></div>
-            </form>
-            <?php 
-                if(isset($_SESSION['blad']))
-                echo $_SESSION['blad']; 
-            
-            
-            
-            ?>
+        <div id="container">
+            <div id="loginform">
+                <form action="zaloguj.php" method="post">
+                    Login lub email: <br/> <input type="text" name="login" /> <br/>
+                    Hasło: <br/> <input type="password" name="password"/> <br/> <br/>
+                    <div id="button" ><input type="submit" value="Zaloguj się"/></div>
+                </form>
+                <?php 
+                    if(isset($_SESSION['blad']))
+                    echo $_SESSION['blad']; 
+                
+                
+                
+                ?>
+            </div>
         </div>
     </body>
 </html>
